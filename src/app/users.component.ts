d5431b05f8a185a1011bb1a45c2cde0712fd3ba9
Greg Lim <limjunqu@gmail.com>
2020-04-03 23:38:26 +0800

    Update users.component.ts

--- src/app/users.component.ts
+++ src/app/users.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
-
-import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument} from 'angularfire2/firestore';
+import {AngularFirestore, AngularFirestoreCollection} from '@angular/fire/firestore';
 import { Router } from '@angular/router';
 import { map } from 'rxjs/operators';
 
@@ -10,32 +9,29 @@ interface User{
 }
 @Component({
     selector: 'users',
-    templateUrl: './users.component.html'  
+    templateUrl: './user.component.html'  
   })
-export class UsersComponent {
-  title: string;
-  content: string;  
+export class UserComponent {
 
   usersCol: AngularFirestoreCollection<User>;  
   users: any;
 
   constructor(private afs: AngularFirestore, private _router: Router){
-
   }
 
   ngOnInit(){
     this.usersCol = this.afs.collection('users');    
-    //this.users = this.usersCol.valueChanges();
     this.users = this.usersCol.snapshotChanges()
-        .pipe(
-            map(actions => {
-                return actions.map( a => {
-                    const data = a.payload.doc.data() as User;
-                    const id = a.payload.doc.id;
-                    return { id, data};
-                });
-            })
-         ); 
+    .pipe(
+        map(actions => {
+            return actions.map( a => {
+                const data = a.payload.doc.data() as User;
+                const id = a.payload.doc.id;
+                return { id, data};
+            });
+        })
+     );  
+
   }
 
   add(){
@@ -47,4 +43,5 @@ export class UsersComponent {
         this.afs.doc('users/'+userId).delete();  
     }                  
   }  
+
 }
