5913c11bff571c8744386788dbfd0ffcd5cb71c8
Greg Lim <limjunqu@gmail.com>
2020-04-03 23:40:55 +0800

    Update user-form.component.ts

--- src/app/user-form.component.ts
+++ src/app/user-form.component.ts
@@ -1,26 +1,26 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { Router, ActivatedRoute } from '@angular/router';
-import { AngularFirestore, AngularFirestoreDocument} from 'angularfire2/firestore';
+import { Router,ActivatedRoute  } from '@angular/router';
+import { AngularFirestore,AngularFirestoreDocument} from '@angular/fire/firestore';
 
 import { User } from './user';
-import { Observable } from 'rxjs';
+import { Observable } from 'rxjs'
 
 @Component({
     selector:'user-form',
     templateUrl: 'user-form.component.html'
 })
-export class UserFormComponent  {        
-    id;
+export class UserFormComponent  {   
+    id;     
     form: FormGroup;  
     title: string;
-    user = new User();    
-
+    user = new User();   
+    
     userDoc: AngularFirestoreDocument<User>;
     singleUser: Observable<User>;      
+    
 
-    constructor(fb: FormBuilder, private _router:Router, private _route:ActivatedRoute,
-private afs: AngularFirestore){        
+    constructor(fb: FormBuilder, private _router:Router, private afs: AngularFirestore, private _route:ActivatedRoute){        
         this.form = fb.group({
             username:['',Validators.required ],
             email:['',Validators.required]            
@@ -33,7 +33,7 @@ private afs: AngularFirestore){
         });      
                 
         if(!this.id){
-            this.title = "New User";            
+            this.title = "New User";                        
         }
         else{
             this.title = "Edit User";                        
@@ -42,11 +42,11 @@ private afs: AngularFirestore){
             this.singleUser.subscribe((user) =>{
                 this.form.get('username').setValue(user.name);                
                 this.form.get('email').setValue(user.email);                                
-            });            
-        }        
+            });                        
+        }               
     }    
 
-    submit(){                        
+    submit(){                                        
         if (this.id) {   
             this.afs.doc('users/'+this.id).update({
                 name: this.user.name,	
@@ -58,8 +58,8 @@ private afs: AngularFirestore){
                 name: this.user.name,	
                 email: this.user.email  
             });                         
-        }
-                                    
+        }                                 
         this._router.navigate(['']);
-    } 
+    }
+
 }
